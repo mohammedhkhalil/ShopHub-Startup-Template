f910c8c87cc5a8ee96c487a6fc71afaa6a4872ee
Mohamed H.Khalil <mohamedhussienkhalil0@gmail.com>
2026-07-12 01:18:18 +0300

    UoW

---
+++ DAL/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿using DAL.Data;
+using DAL.IRepositories;
+using DAL.Repositories;
+
+namespace DAL.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+        public ICategoryRepository Category { get; private set; }
+        public IProductRepository Product { get; private set; }
+        public IOrderHeaderRepository OrderHeader { get; private set; }
+        public IOrderDetailRepository OrderDetail { get; private set; }
+        public IShoppingCartRepository ShoppingCart { get; private set; }
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+            Category = new CategoryRepository(_context);
+            Product = new ProductRepository(_context);
+            OrderHeader = new OrderHeaderRepository(_context);
+            OrderDetail = new OrderDetailRepository(_context);
+            ShoppingCart = new ShoppingCartRepository(_context);
+        }
+
+        public void Dispose()
+        {
+            _context?.Dispose();
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
--- DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
+++ DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13deb8d612a988164e0ad4c23945c4dd574446f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b275533e68c55a96fd130c163157a2d2816647f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
